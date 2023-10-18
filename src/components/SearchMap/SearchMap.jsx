502a222456024a3e69619374864cf728ee4a36dc
halziebol11 <halziebol11@gmail.com>
2023-10-18 13:03:00 -0500

    updated API key

--- src/components/SearchMap/SearchMap.jsx
+++ src/components/SearchMap/SearchMap.jsx
@@ -8,7 +8,7 @@ function SearchMap() {
 
   const { isLoaded } = useLoadScript({
 
-  googleMapsApiKey: 'AIzaSyDKZ7CqOhTLEdvHJjoqrfeaa8OoJVFy3co',
+  googleMapsApiKey: 'api',
   });
   const mapStyle = {        
     height: "50vh",
